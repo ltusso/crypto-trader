fc809e1df06507aded04eec0eafb2524559d8f47
Leo Tusso <leo.tusso@greyfinch.com>
2020-06-23 21:34:46 +0200

    Add some crypto fake data to our Database

--- crypto-trader-server/src/main/resources/data.sql
+++ crypto-trader-server/src/main/resources/data.sql
@@ -1,1 +1,7 @@
-insert into Customer (name,last_name,budget) values ('leo','tusso',90000);
+insert into Customer (name,last_name,budget) values ('leo','tusso',25000);
+
+insert into Crypto(code,name,description,price) values ('bitcoin','BTC','Bitcoin',9671.34);
+insert into Crypto(code,name,description,price) values ('ethereum','ETH','Ethereum',244.54);
+insert into Crypto(code,name,description,price) values ('tether','USDT','Tether',1.0089);
+insert into Crypto(code,name,description,price) values ('ripple','XRP','Ripple',0.189);
+insert into Crypto(code,name,description,price) values ('bitcoin-cash','BCH','Bitcoin Cash',243.19);
