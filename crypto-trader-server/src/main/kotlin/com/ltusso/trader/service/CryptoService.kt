79ec14071bac51c8402f4ffda7d7ac087b04437b
Leo Tusso <leo.tusso@greyfinch.com>
2020-06-23 21:35:38 +0200

    add crypto service.

--- crypto-trader-server/src/main/kotlin/com/ltusso/trader/service/CryptoService.kt
+++ crypto-trader-server/src/main/kotlin/com/ltusso/trader/service/CryptoService.kt
@@ -4,6 +4,7 @@ import com.ltusso.trader.model.Crypto
 import com.ltusso.trader.repository.CryptoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class CryptoService(
@@ -14,6 +15,13 @@ class CryptoService(
 
     fun getCryptoById(code: String): Crypto? {
         return cryptoRepository.findByCode(code)
+    }
+
+    fun addCrypto(code: String, price: BigDecimal, name: String): Crypto {
+        if (getCryptoById(code) != null) {
+            throw RuntimeException("Crypto $code already exists")
+        }
+        return cryptoRepository.save(Crypto(name = name, code = code, price = price))
     }
 
 }
