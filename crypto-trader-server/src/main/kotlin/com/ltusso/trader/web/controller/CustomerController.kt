c61f35197f0bcf280e16c94f2c4924168bec7cdf
Leo Tusso <leo.tusso@greyfinch.com>
2020-06-23 20:14:14 +0200

    Add ResponseEntity as return type on CustomerController

--- crypto-trader-server/src/main/kotlin/com/ltusso/trader/web/controller/CustomerController.kt
+++ crypto-trader-server/src/main/kotlin/com/ltusso/trader/web/controller/CustomerController.kt
@@ -4,7 +4,9 @@ import com.ltusso.trader.service.CustomerService
 import com.ltusso.trader.web.dto.CryptoDTO
 import com.ltusso.trader.web.dto.CustomerDTO
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,16 +16,36 @@ class CustomerController(
 ) {
 
     @RequestMapping
-    fun findAll(): List<CustomerDTO> {
-        return customerService.findAll()
+    fun findAll(): ResponseEntity<List<CustomerDTO>> {
+        return ResponseEntity.ok(customerService.findAll()
                 .map { customer ->
                     val cryptoInfoPurchases = customer.purchases
-                            .map { CustomerDTO.CryptoInfoDTO(
-                                    cryptoDTO = CryptoDTO(it.crypto.name, it.crypto.code),
-                                    amount = it.purchasedAmount,
-                                    totalPrice = it.price)
+                            .map {
+                                CustomerDTO.CryptoInfoDTO(
+                                        cryptoDTO = CryptoDTO(it.crypto.name, it.crypto.code),
+                                        amount = it.purchasedAmount,
+                                        totalPrice = it.price)
                             }
                     CustomerDTO(customer.name, customer.lastName, customer.budget, cryptoInfoPurchases)
                 }
+        )
     }
+
+    @RequestMapping("/{customerId}")
+    fun find(@RequestParam customerId: Long): ResponseEntity<CustomerDTO> {
+        val customer = customerService.findById(customerId)
+
+        if (customer != null) {
+            return ResponseEntity.ok(CustomerDTO(customer.name, customer.lastName, customer.budget, customer.purchases
+                    .map {
+                        CustomerDTO.CryptoInfoDTO(
+                                cryptoDTO = CryptoDTO(it.crypto.name, it.crypto.code),
+                                amount = it.purchasedAmount,
+                                totalPrice = it.price)
+                    }))
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+
 }
