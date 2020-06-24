7343301ba728975b0c0dc2a8b0715de758197835
Leo Tusso <leo.tusso@greyfinch.com>
2020-06-24 21:00:49 +0200

    remove useless state

--- crypto-trader-ui/src/App.js
+++ crypto-trader-ui/src/App.js
@@ -2,13 +2,6 @@ import React, { Component } from "react";
 import CoinListComponent from "./components/coin_list_component/CoinListComponent";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cryptos: [],
-    };
-  }
-
   render() {
     return <CoinListComponent />;
   }
