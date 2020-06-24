6293f377ebe41a4cdf33d91ec5a2ae044daa0f39
Leo Tusso <leo.tusso@greyfinch.com>
2020-06-24 23:20:00 +0200

    Add Bootstrap table to show customer operation

--- crypto-trader-ui/src/components/user_detail_component/UserDetailComponent.jsx
+++ crypto-trader-ui/src/components/user_detail_component/UserDetailComponent.jsx
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
-
+import { Table } from "react-bootstrap";
 class UserDetailComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       customerId: this.props.customerId,
+      purchases: [],
     };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:8080/customer/" + this.props.customerId)
+      .then((response) => response.json())
+      .then((x) =>
+        this.setState({
+          purchases: x.purchases.map((i) => ({
+            amount: i.amount,
+            price: i.totalPrice,
+            crypto: {
+              name: i.cryptoDTO.name,
+              id: i.cryptoDTO.id,
+            },
+          })),
+        })
+      );
+  }
+
   render() {
-    return <div> My operations are shown here.</div>;
+    /*  return this.state.purchases.map((purchase) => {
+      return <span>{purchase.crypto.id}</span>;   
+    });
+    */
+
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Coin id</th>
+            <th>Amount Purchased</th>
+            <th>Total Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.purchases.map((purchase) => {
+            return (
+              <tr>
+                <td>{purchase.crypto.id}</td>
+                <td>{purchase.amount}</td>
+                <td>{purchase.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    );
   }
 }
 export default UserDetailComponent;
